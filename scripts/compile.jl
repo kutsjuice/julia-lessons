171b62d26ef07c1b7148f5b9b75b6528708fe3c0
Kuts Michael <kutsjuice@gmail.com>
2025-03-14 16:11:16 +0300

    adds compile script and project structure

---
+++ scripts/compile.jl
@@ -0,0 +1,11 @@
+using Literate
+
+@__DIR__
+for dir in readdir((@__DIR__)*"\\..\\src", join=true)
+    println(dir)
+    for file in readdir(dir)
+        splitext(file)[2] == ".jl" || continue
+        Literate.markdown(dir*"\\"*file, "compiled/lesson_2"; execute=true, documenter=false)
+        println(file)
+    end
+end
