77ee02f9f0da40c6209d46612b71ce43d6eacf53
Kuts Michael <kutsjuice@gmail.com>
2025-03-14 16:30:58 +0300

    fixes compile script

--- scripts/compile.jl
+++ scripts/compile.jl
@@ -3,6 +3,7 @@ using Literate
 @__DIR__
 for dir in readdir((@__DIR__)*"\\..\\src", join=true)
     println(dir)
+    isdir(dir) || continue
     for file in readdir(dir)
         splitext(file)[2] == ".jl" || continue
         Literate.markdown(dir*"\\"*file, "compiled/lesson_2"; execute=true, documenter=false)
